c9f1ca278ecc7bbe44ec24af5e5767fdcc2c580e
HamdanUbaidillah <daniuyan71@gmail.com>
2022-06-22 09:25:36 +0700

    changes

--- app.js
+++ app.js
@@ -3,24 +3,36 @@ const max = document.querySelector(".max");
 const output = document.querySelector(".output");
 const generate = document.querySelector(".generate");
 
-generate.addEventListener("click", genNumber);
+// generate.addEventListener("click", function () {
+//   if (min.value > max.value) {
+//     [max.value, min.value] = [min.value, max.value];
+//   }
+//   let start = new Date().getTime();
+//   setInterval(() => {
+//     if (new Date().getTime() - start > 500) {
+//       clearInterval;
+//       return;
+//     } else {
+//       let range = max.value - min.value;
+//       let random = Math.floor(Math.random() * (range + 1)) + parseInt(min.value);
+//       output.innerHTML = random;
+//     }
+//   }, 20);
+// });
 
-function genNumber() {
-  if (min.value == "" && max.value == "") {
-    output.innerHTML = "0";
-  } else if (min.value > max.value) {
-    [max.value, min.value] = [min.value, max.value];
-  } else {
-    let start = new Date().getTime();
-    setInterval(() => {
-      if (new Date().getTime() - start > 500) {
-        clearInterval;
-        return;
-      } else {
-        let range = max.value - min.value;
-        let random = Math.floor(Math.random() * (range + 1)) + parseInt(min.value);
-        output.innerHTML = random;
+generate.addEventListener("click", getRandom);
+
+function getRandom() {
+  let start = new Date().getTime();
+  setInterval(() => {
+    if (new Date().getTime() - start > 500) {
+      clearInterval;
+      return;
+    } else {
+      randomNum = Math.floor(Math.random() * parseInt(max.value)) + 1;
+      if (randomNum >= parseInt(min.value)) {
+        output.innerHTML = randomNum;
       }
-    }, 20);
-  }
+    }
+  }, 20);
 }
