615bc15315cc63dbf0e48e66205dc7bf4ac849ef
HamdanUbaidillah <daniuyan71@gmail.com>
2022-06-22 09:26:09 +0700

    delete comments

--- app.js
+++ app.js
@@ -3,23 +3,6 @@ const max = document.querySelector(".max");
 const output = document.querySelector(".output");
 const generate = document.querySelector(".generate");
 
-// generate.addEventListener("click", function () {
-//   if (min.value > max.value) {
-//     [max.value, min.value] = [min.value, max.value];
-//   }
-//   let start = new Date().getTime();
-//   setInterval(() => {
-//     if (new Date().getTime() - start > 500) {
-//       clearInterval;
-//       return;
-//     } else {
-//       let range = max.value - min.value;
-//       let random = Math.floor(Math.random() * (range + 1)) + parseInt(min.value);
-//       output.innerHTML = random;
-//     }
-//   }, 20);
-// });
-
 generate.addEventListener("click", getRandom);
 
 function getRandom() {
